3a5fdd2a8ac1e85ffbfaf4c1d086c345bab8a708
Abdullah Saleem <a.saleem2993@gmail.com>
2019-05-16 20:31:39 +0500

    min and max length added

--- brute.go.go
+++ brute.go.go
@@ -1,24 +1,28 @@
 package brute
 
 // Brute returns a channel of all possible combinations of a characterset.
-// charset is the characterset to be used, length is the maximum length of
-// combinations, buffer specifies the buffer size of the returned channel,
-// fixed indicates whether or not the cominations whould of the fixed size i.e. length
-// if false then it will retuen all combinations of size 1 upto length.
+// charset is the characterset to be used, minLen is the minimum length of
+// combinations and should be greater than 0 it will panic otherwise, maxLen is the
+// maxmimum length of combinations and should be greater than or equal to minLen, otherwise it will panic
+// buffer specifies the buffer size of the returned channel,
 // closer can be called to terminte the generator
-func Brute(charset []rune, length, buffer int, fixed bool) (combos <-chan string, closer func()) {
+func Brute(charset []rune, minLen, maxLen, buffer int) (combos <-chan string, closer func()) {
+
 	results := make(chan string, buffer)
 	done := make(chan struct{})
 	charlen := len(charset)
 
-	m := 1
-	if fixed {
-		m = length
+	if minLen < 1 {
+		panic("min length should be greater than 0")
+	}
+
+	if minLen > maxLen {
+		panic("max length should be greater than max length")
 	}
 
 	go func() {
 		defer close(results)
-		for k := m; k <= length; k++ {
+		for k := minLen; k <= maxLen; k++ {
 			carry := 0
 			indices := make([]int, k)
 			for {
